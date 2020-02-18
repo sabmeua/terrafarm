f1081a1786362f48deee1eaf3a5e22ab5282c0f6
sabmeua <sabme.ua@gmail.com>
2020-02-18 16:26:35 +0900

    Add route53 cname record example

---
+++ aws/route53/private_cname.tf
@@ -0,0 +1,12 @@
+data "aws_route53_zone" "local" {
+  name         = "local."
+  private_zone = true
+}
+
+resource "aws_route53_record" "rds_app_db" {
+  zone_id = "${data.aws_route53_zone.local.zone_id}"
+  name    = "${var.app_name}-db.${data.aws_route53_zone.local.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_db_instance.app_db.address}"]
+}
